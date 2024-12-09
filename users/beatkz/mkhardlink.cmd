669459be4a9e6573e25f6c64da71164bd809378c
Keiji Momose <keiji.momose@soliton.co.jp>
2024-12-09 17:51:21 +0900

    [nicola+via] added: for code DRYing, hard-link script for Windows.

---
+++ users/beatkz/mkhardlink.cmd
@@ -0,0 +1,6 @@
+SET TARGET=%1
+
+mklink /h %TARGET%\keymaps\nicola_via\nicola.h .\nicola.h
+mklink /h %TARGET%\keymaps\nicola_via\nicola.c .\nicola.c
+mklink /h %TARGET%\keymaps\nicola_via\key_duration.h .\key_duration.h
+mklink /h %TARGET%\keymaps\nicola_via\key_duration.c .\key_duration.c
