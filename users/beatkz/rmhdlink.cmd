0f9a543f3e45c8df4cb2cb82d520d34759704b2b
Keiji Momose <keiji.momose@soliton.co.jp>
2025-09-18 11:37:01 +0900

    コミット用にnicola.c,key_duration.c,key_duration_arm.cを含めないためのバッチファイルを作成

---
+++ users/beatkz/rmhdlink.cmd
@@ -0,0 +1,8 @@
+@echo off
+SET TARGET=%1
+
+del %TARGET%\keymaps\nicola_via\nicola.h
+del %TARGET%\keymaps\nicola_via\nicola.c
+del %TARGET%\keymaps\nicola_via\key_duration.h
+del %TARGET%\keymaps\nicola_via\key_duration.c
+del %TARGET%\keymaps\nicola_via\key_duration_arm.c
