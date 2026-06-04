9405b2b9fa41c451592b7f04e3ecea781e35e1bb
Keiji Momose <keiji.momose@gmail.com>
2026-06-04 12:04:19 +0900

    renewQMKrepo.shをパッケージマネージャーベースのオプションに変更

--- renewQMKrepo.sh
+++ renewQMKrepo.sh
@@ -2,9 +2,9 @@
 
 if [ "$#" -ne 1 ]; then
     echo "usage: ./renewQMKrepo.sh -amdcg"
-    echo "-a -> Update Debian based Distro(apt)"
-    echo "-m -> Update MSYS(for Windows Build Environment)"
+    echo "-a -> Update Debian based Distro(apt)[for QMK-WSL]"
     echo "-d -> Update RHEL based Distro(dnf)"
+    echo "-p -> Update Arch based Distro(pacman)[for QMK-MSYS]"
     echo "-c -> Update QMK CLI"
     echo "-g -> Update QMK Git Repositry(add official repo as 'upstream' first)"
     echo "-v -> Update VIA's Userspace QMK Git Repositry(add official repo as 'upstream' first)"
@@ -13,19 +13,19 @@ if [ "$#" -ne 1 ]; then
 fi
 
 UPD_APT=FALSE
-UPD_MSYS=FALSE
 UPD_DNF=FALSE
+UPD_PACMAN=FALSE
 UPD_CLI=FALSE
 UPD_GIT=FALSE
 UPD_US_VIA=FALSE
 
 OPT=
 OPTIND=
-while getopts acdgmv OPT ; do
+while getopts adpcgmv OPT ; do
     case $OPT in
         a) UPD_APT=TRUE; ;;
-        m) UPD_MSYS=TRUE; ;;
         d) UPD_DNF=TRUE; ;;
+        p) UPD_PACMAN=TRUE; ;;
         c) UPD_CLI=TRUE; ;;
         g) UPD_GIT=TRUE; ;;
         v) UPD_US_VIA=TRUE; ;;
@@ -38,14 +38,14 @@ if [ $UPD_APT = "TRUE" ] ; then
     sudo apt update && sudo apt -y upgrade
 fi
 
-if [ $UPD_MSYS = "TRUE" ] ; then
-    #Update for QMK MSYS/MSYS
-    pacman -Syu --noconfirm
-fi
-
 if [ $UPD_DNF = "TRUE" ] ; then
     #Update for RHEL based Distro
     sudo dnf -y update
+fi
+
+if [ $UPD_PACMAN = "TRUE" ] ; then
+    #Update for Arch based Distro
+    pacman -Syu --noconfirm
 fi
 
 if [ $UPD_CLI = "TRUE" ] ; then
@@ -56,12 +56,12 @@ fi
 if [ $UPD_GIT = "TRUE" ] ; then
     #Update for QMK Repo
     git fetch --all
-    git merge --no-edit upstream/master
+    git merge --no-edit --ff-only upstream/master
     make git-submodule
 fi
 
 if [ $UPD_US_VIA = "TRUE" ] ; then
     #Update for VIA's Userspace QMK Git Repo
     git fetch --all
-    git merge --no-edit upstream/main
+    git merge --no-edit --ff-only upstream/main
 fi
