7807bac29eabd6bb7f3a379875c10dd48acbe7df
Keiji Momose <keiji.momose@gmail.com>
2026-08-27 15:14:32 +0900

    yushakobo/primer61: - タイプカウントの加算処理の付け忘れを修正。 - ホーム行をメジャースケールに、下段行をマイナースケールにした音階定義を追加。 - トランポーズの表示を音階名に変更。 - Shift押しで音の強弱の切替を追加。

--- keyboards/yushakobo/primer61/keymaps/midi_via_kb2040/keymap.c
+++ keyboards/yushakobo/primer61/keymaps/midi_via_kb2040/keymap.c
@@ -15,7 +15,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______,   TO(2), _______, _______, _______,          _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______
+    ),
+    // MIDI Layer (Home Row: C Major, Lower Row: C Minor)
+    [2] = LAYOUT_60_ansi(
+          TO(0), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, MI_OCTU, MI_TRSU, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, MI_OCTD, MI_TRSD, _______,
+        _______,   MI_C3,   MI_D3,   MI_E3,   MI_F3,   MI_G3,   MI_A3,   MI_B3,   MI_C4,   MI_D4,   MI_E4,   MI_F4,          _______,
+        _______,            MI_C3,   MI_D3,  MI_Eb3,   MI_F3,   MI_G3,  MI_Ab3,  MI_Bb3,   MI_C4,   MI_D4,  MI_Eb4,          _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
 };
@@ -58,10 +66,21 @@ void oled_write_host_led_state(void) {
     oled_write_ln_P(led_state.scroll_lock ? PSTR("On") : PSTR("- "), false);
 }
 
-// MIDI状態の出力関数
-static void oled_write_midi_status(void) {
+// 音階テーブル (PROGMEMに保持)
+// マイナス側：Gb, G, Ab, A, Bb, B
+// 基準   ：C
+// プラス側 ：C#, D, D#, E, F, F#
+static const char transpose_names[13][4] PROGMEM = {
+    "Gb", "G ", "Ab", "A ", "Bb", "B ",
+    "C ",
+    "C#", "D ", "D#", "E ", "F ", "F#"
+};
+
+static void render_midi_status(void) {
 #ifdef MIDI_ADVANCED
     char buf[8];
+
+    bool is_accent = (get_mods() & MOD_MASK_SHIFT) || (get_oneshot_mods() & MOD_MASK_SHIFT);
 
     // --- Octave ---
     oled_write_P(PSTR("O:"), false);
@@ -69,10 +88,17 @@ static void oled_write_midi_status(void) {
     oled_write(buf, false);
     oled_write_P(PSTR("\n"), false);
 
-    // --- Transposition ---
+    // --- Transposition (修正した音階テーブル適用) ---
     oled_write_P(PSTR("T:"), false);
-    itoa(midi_config.transpose, buf, 10);
-    oled_write(buf, false);
+
+    // transpose (-6 ~ +6) を配列インデックス (0 ~ 12) に変換
+    int8_t tr = midi_config.transpose;
+    if (tr < -6) tr = -6;
+    if (tr > 6)  tr = 6;
+    uint8_t idx = tr + 6;
+
+    // テーブルから音名を出力
+    oled_write_P(transpose_names[idx], false);
     oled_write_P(PSTR("\n"), false);
 
     // --- Velocity ---
@@ -80,6 +106,14 @@ static void oled_write_midi_status(void) {
     itoa(midi_config.velocity, buf, 10);
     oled_write(buf, false);
     oled_write_P(PSTR("\n"), false);
+
+    // --- Accent Indicator ---
+    if (is_accent) {
+        oled_write_P(PSTR("ACC!\n"), true);
+    } else {
+        oled_write_P(PSTR("    \n"), false);
+    }
+
 #else
     oled_write_P(PSTR("MIDI\nOFF\n"), false);
 #endif
@@ -100,8 +134,17 @@ void oled_write_type_count_portrait(void) {
 bool oled_task_user(void) {
     oled_write_layer_state();
     oled_write_host_led_state();
-    oled_write_midi_status();
+    render_midi_status();
     oled_write_type_count_portrait();
     return false;
 }
 #endif // OLED_ENABLE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef OLED_ENABLE
+    if (record->event.pressed) {
+        count_type();
+    }
+#endif
+    return true;
+}
